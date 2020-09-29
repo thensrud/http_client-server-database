70aee171058d0cede1196ee0be507f3d6e068378
Fredrik Holm Eikrem <fredrik_eikrem@hotmail.com>
2020-09-29 19:59:12 +0200

    shouldReturnContentLength green

--- pgr203innlevering2-thensrud/src/test/java/no/kristiania/http/HttpServerTest.java
+++ pgr203innlevering2-thensrud/src/test/java/no/kristiania/http/HttpServerTest.java
@@ -1,4 +1,4 @@
-package no.kristinia.http;
+package no.kristiania.http;
 
 import org.junit.jupiter.api.Test;
 
@@ -21,4 +21,12 @@ class HttpServerTest {
         HttpClient client = new HttpClient("localhost", 10002, "/echo?status=404");
         assertEquals(404, client.getStatusCode());
     }
+
+    @Test
+    void shouldReturnContentLength() throws IOException {
+        new HttpServer(10003);
+        HttpClient client = new HttpClient("localhost", 10003, "/echo?body=HelloWorld");
+        assertEquals("10", client.getResponseHeader("Content-Length"));
+    }
+
 }
