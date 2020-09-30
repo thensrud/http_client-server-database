8c4180738b3537a7c59b615593481e276e2ee12a
Fredrik Holm Eikrem <fredrik_eikrem@hotmail.com>
2020-09-30 13:25:42 +0200

    localhost:8080/index.html vil ikke åpnes i webserver ...

---
+++ pgr203innlevering2-thensrud/src/main/java/no/kristiania/http/HttpServer.java
@@ -0,0 +1,82 @@
+package no.kristiania.http;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer {
+
+    private File contentRoot;
+
+    public HttpServer(int port) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+
+        new Thread(() -> {
+            while (true) {
+                try {
+                    handleRequest(serverSocket.accept());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void handleRequest(Socket clientSocket) throws IOException {
+        String requestLine = HttpClient.readLine(clientSocket);
+        System.out.println(requestLine);
+
+        String requestTarget = requestLine.split(" ")[1];
+        String statusCode = "200";
+        String body = "Hello <strong>World</strong>";
+
+        int questionPos = requestTarget.indexOf('?');
+        if (questionPos != -1) {
+            QueryString queryString = new QueryString(requestTarget.substring(questionPos+1));
+            if (queryString.getParameter("status") != null){
+                statusCode = queryString.getParameter("status");
+            }
+            if (queryString.getParameter("body") != null){
+
+                body = queryString.getParameter("body");
+
+            }
+        } else {
+            File file = new File(contentRoot, requestTarget);
+            statusCode = "200";
+            String response = "HTTP/1.1 " + statusCode + " OK\r" +
+                    "\n" +
+                    "Content-Length: " + file.length() + "\r\n" +
+                    "Content-Type: text/plain\r\n"+
+                    "\r\n";
+
+            clientSocket.getOutputStream().write(response.getBytes());
+
+            new FileInputStream(file).transferTo(clientSocket.getOutputStream());
+
+
+        }
+
+
+        String response = "HTTP/1.1 " + statusCode + " OK\r" +
+                "\n" +
+                "Content-Length: " + body.length() + "\r\n" +
+                "Content-Type: text/plain\r\n"+
+                "\r\n"+
+                body;
+
+
+        clientSocket.getOutputStream().write(response.getBytes());
+    }
+
+    public static void main (String[] args) throws IOException {
+        HttpServer server = new HttpServer(8080);
+        server.setContentRoot(new File("src/main/resources"));
+    }
+
+    public void setContentRoot(File contentRoot) {
+        this.contentRoot = contentRoot;
+    }
+}
