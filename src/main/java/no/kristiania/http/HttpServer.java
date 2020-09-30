15e420659b07927d38078b2d75c871015cd4628a
thensrud <48883224+thensrud@users.noreply.github.com>
2020-09-30 21:37:46 +0200

    Merge

---
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -0,0 +1,136 @@
+package no.kristiania.http;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class HttpServer {
+
+    private File contentRoot;
+    private List<String> workerNames = new ArrayList<>();
+
+    public HttpServer(int port) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Socket clientSocket = serverSocket.accept();
+                    handleRequest(clientSocket);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void handleRequest(Socket clientSocket) throws IOException {
+        HttpMessage request = new HttpMessage(clientSocket);
+        String requestLine = request.getStartLine();
+        System.out.println(requestLine);
+
+        String requestMethod = requestLine.split(" ")[0];
+        String requestTarget = requestLine.split(" ")[1];
+
+        int questionPos = requestTarget.indexOf('?');
+
+        String requestPath = questionPos != -1 ? requestTarget.substring(0, questionPos) : requestTarget;
+
+        if (requestMethod.equals("POST")) {
+            QueryString requestParameter = new QueryString(request.getBody());
+
+            workerNames.add(requestParameter.getParameter("full_name"));
+            String body = "Okay";
+            String response = "HTTP/1.1 200 OK\r\n" +
+                    "Content-Length: " + body.length() + "\r\n" +
+                    "\r\n" +
+                    body;
+            clientSocket.getOutputStream().write(response.getBytes());
+        } else {
+            if (requestPath.equals("/echo")) {
+                handleEchoRequest(clientSocket, requestTarget, questionPos);
+            } else if (requestPath.equals("/api/workers")) {
+                handleGetWorker(clientSocket);
+            } else {
+                File file = new File(contentRoot, requestPath);
+                if (!file.exists()) {
+                    String body = file + " does not exist";
+                    String response = "HTTP/1.1 404 Not Found\r\n" +
+                            "Content-Length: " + body.length() + "\r\n" +
+                            "\r\n" +
+                            body;
+                    clientSocket.getOutputStream().write(response.getBytes());
+                    return;
+                }
+                String statusCode = "200";
+                String contentType = "text/plain";
+                if (file.getName().endsWith(".html")) {
+                    contentType = "text/html";
+                }
+                if (file.getName().endsWith(".css")) {
+                    contentType = "text/css";
+                }
+                String response = "HTTP/1.1 " + statusCode + " OK\r\n" +
+                        "Content-Length: " + file.length() + "\r\n" +
+                        "Content-Type: " + contentType + "\r\n" +
+                        "\r\n";
+                clientSocket.getOutputStream().write(response.getBytes());
+
+                new FileInputStream(file).transferTo(clientSocket.getOutputStream());
+            }
+        }
+    }
+
+    private void handleGetWorker(Socket clientSocket) throws IOException {
+        String body = "<ul>";
+        for (String workerName : workerNames) {
+            body += "<li>" + workerName + "</li>";
+        }
+        body += "</ul>";
+        String response = "HTTP/1.1 200 OK\r\n" +
+                "Content-Length: " + body.length() + "\r\n" +
+                "Content-Type: text/plain\r\n" +
+                "\r\n" +
+                body;
+
+        clientSocket.getOutputStream().write(response.getBytes());
+    }
+
+    private void handleEchoRequest(Socket clientSocket, String requestTarget, int questionPos) throws IOException {
+        String statusCode = "200";
+        String body = "Hello <strong>World</strong>!";
+        if (questionPos != -1) {
+            QueryString queryString = new QueryString(requestTarget.substring(questionPos + 1));
+            if (queryString.getParameter("status") != null) {
+                statusCode = queryString.getParameter("status");
+            }
+            if (queryString.getParameter("body") != null) {
+                body = queryString.getParameter("body");
+            }
+        }
+        String response = "HTTP/1.1 " + statusCode + " OK\r\n" +
+                "Content-Length: " + body.length() + "\r\n" +
+                "Content-Type: text/plain\r\n" +
+                "\r\n" +
+                body;
+
+        clientSocket.getOutputStream().write(response.getBytes());
+    }
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = new HttpServer(8080);
+        server.setContentRoot(new File("pgr203innlevering2-thensrud/src/main/resources"));
+    }
+
+    public void setContentRoot(File contentRoot) {
+        this.contentRoot = contentRoot;
+    }
+
+    public List<String> getWorkerNames() {
+        return workerNames;
+    }
+}
---
+++ src/test/java/no/kristiania/http/HttpServerTest.java
@@ -0,0 +1,95 @@
+package no.kristiania.http;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HttpServerTest {
+
+    @Test
+    void shouldReturnSuccessfulStatusCode() throws IOException {
+        new HttpServer(10001);
+        HttpClient client = new HttpClient("localhost", 10001, "/echo");
+        assertEquals(200, client.getStatusCode());
+    }
+
+    @Test
+    void shouldReturnUnsuccessfulStatusCode() throws IOException {
+        new HttpServer(10002);
+        HttpClient client = new HttpClient("localhost", 10002, "/echo?status=404");
+        assertEquals(404, client.getStatusCode());
+    }
+
+    @Test
+    void shouldReturnContentLength() throws IOException {
+        new HttpServer(10003);
+        HttpClient client = new HttpClient("localhost", 10003, "/echo?body=HelloWorld");
+        assertEquals("10", client.getResponseHeader("Content-Length"));
+    }
+
+    @Test
+    void shouldReturnResponseBody() throws IOException {
+        new HttpServer(10004);
+        HttpClient client = new HttpClient("localhost", 10004, "/echo?body=HelloWorld");
+        assertEquals("HelloWorld", client.getResponseBody());
+    }
+
+    @Test
+    void shouldReturnFileFromDisk() throws IOException {
+        HttpServer server = new HttpServer(10005);
+        File contentRoot = new File("target/");
+        server.setContentRoot(contentRoot);
+
+        String fileContent = "Hello World" + new Date();
+        Files.writeString(new File(contentRoot, "test.txt").toPath(), fileContent);
+
+        HttpClient client = new HttpClient("localhost", 10005, "/test.txt");
+        assertEquals(fileContent, client.getResponseBody());
+        assertEquals("text/plain", client.getResponseHeader("Content-Type"));
+    }
+
+    @Test
+    void shouldReturnCorrectContentType() throws IOException {
+        HttpServer server = new HttpServer(10006);
+        File contentRoot = new File("target/");
+        server.setContentRoot(contentRoot);
+
+        Files.writeString(new File(contentRoot, "index.html").toPath(), "<h2>Hello World</h2>");
+
+        HttpClient client = new HttpClient("localhost", 10006, "/index.html");
+        assertEquals("text/html", client.getResponseHeader("Content-Type"));
+    }
+
+    @Test
+    void shouldReturn404IfFileNotFound() throws IOException {
+        HttpServer server = new HttpServer(10007);
+        File contentRoot = new File("target/");
+        server.setContentRoot(contentRoot);
+
+        HttpClient client = new HttpClient("localhost", 10007, "/notFound.txt");
+        assertEquals(404, client.getStatusCode());
+    }
+
+    @Test
+    void shouldPostNewWorker() throws IOException {
+        HttpServer server = new HttpServer(10008);
+        HttpClient client = new HttpClient("localhost", 10008, "/api/newWorker", "POST", "full_name=Thorstein&email_address=abc@hotmail.dsf");
+        assertEquals(200, client.getStatusCode());
+        assertEquals(List.of("Thorstein"), server.getWorkerNames());
+    }
+
+
+    @Test
+    void shouldReturnExistingWorkers() throws IOException {
+        HttpServer server = new HttpServer(10009);
+        server.getWorkerNames().add("Thorstein");
+        HttpClient client = new HttpClient("localhost", 10009, "/api/workers");
+        assertEquals("<ul><li>Thorstein</li></ul>", client.getResponseBody());
+    }
+}
