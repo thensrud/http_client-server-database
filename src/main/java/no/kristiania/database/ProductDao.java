1c913d0108eae1f126ed3b422f903657ec232463
Thorstein Hagen Ensrud <thensrud@hotmail.com>
2020-10-14 10:55:15 +0200

    Product DAO should list inserted products (failing)

--- src/main/java/no/kristiania/database/ProductDao.java
+++ src/main/java/no/kristiania/database/ProductDao.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Scanner;
 
 public class ProductDao {
@@ -37,5 +38,12 @@ public class ProductDao {
                 }
             }
         }
+    }
+
+    public void insert(String product) {
+    }
+
+    public List<String> list() {
+        return null;
     }
 }
